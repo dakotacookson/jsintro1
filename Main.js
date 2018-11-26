91f011961abe07bea24d0e452a7646ba59cf69d0
Dakota J Cookson <44979698+dakotacookson@users.noreply.github.com>
2018-11-26 12:53:27 -0600

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,18 @@
+
+var sectionEl = document.querySelector(".article__header")
+sectionEl.textContent="Welcome to Dakotas Blog";
+console.log(sectionEl);
+
+var sectionE2 = document.querySelector(".article__footer")
+sectionE2.classList.add("Goldenrod");
+console.log(sectionE2);
+
+
+var sectionE3 = document.querySelector(".article__header")
+sectionE3.classList.add("important");
+console.log(sectionE3);
+
+
+var sectionE4 = document.querySelector(".dashed")
+sectionE4.classList=("");
+console.log(sectionE4);
